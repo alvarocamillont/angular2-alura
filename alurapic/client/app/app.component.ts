616347edebb457af8d3035326e08e91662c56104
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-08-18 09:30:02 -0300

    Implementação do primeiro módulo

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
 	selector: 'app',
 	templateUrl: './app/app.component.html'
 })
 
-class AppComponent {}
+export class AppComponent {}
