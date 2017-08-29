bf701ef85f929998b74b57c6c58d55d6180455ab
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-08-29 08:43:03 -0300

    Criação do componente de cadastro

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,8 +1,23 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
     templateUrl: './cadastro.component.html' 
 })
-export class CadastroComponent { }
+export class CadastroComponent { 
+
+    foto: FotoComponent = new FotoComponent();
+
+    constructor(){
+        this.foto.titulo = ''
+        this.foto.url= ''
+        this.foto.descricao= ' '
+    }
+
+    cadastrar(event) {
+        event.preventDefault()
+        console.log(this.foto);
+    }
+}
