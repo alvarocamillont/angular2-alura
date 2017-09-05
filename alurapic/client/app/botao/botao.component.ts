65f1937cbb7e5ea6f0f361b46187cb9e1f9d1862
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-09-05 08:51:56 -0300

    Componente de botão

---
+++ alurapic/client/app/botao/botao.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'botao',
+    templateUrl: './botao.component.html'
+})
+export class BotaoComponent {
+
+    @Input() nome: string = 'Ok';
+    @Input() estilo: string = 'btn-default';
+    @Input() tipo: string = 'button';
+    @Input() desabilitado: boolean = false;
+}
